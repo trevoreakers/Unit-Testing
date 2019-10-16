9c16e98d254ca2f0bbd17a51908e09084279e90d
Trevor Akers <trevor.e.akers-1@ou.edu>
2019-10-16 13:51:10 -0500

    Add student class and test for student

---
+++ StudentTest.java
@@ -0,0 +1,63 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+	private static final int INITIAL_RATING = 5;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testStudent() {
+		try {
+			Student s = new Student();
+
+			Integer rating = s.getRating();
+			Integer expected = INITIAL_RATING;
+
+			assertEquals(expected, rating);
+		} catch (NameException e) {
+			fail();
+		}
+	}
+
+	@Test
+	public void testCompareTo() throws NameException {
+		Student firstStudent = new Student("x", "Y", 2);
+		Student secondStudent = new Student("x", "Y", 3);
+		
+		int actual = secondStudent.compareTo(firstStudent);
+		int expected = 1;
+		
+		assertEquals(expected, actual);
+		
+		Student student = new Student("x", "Y", 5);
+		
+		expected = -1;
+		actual = secondStudent.compareTo(student);
+		assertEquals(expected, actual);
+
+
+		
+		
+		
+
+
+	}
+	
+	@Test
+	public void testGet() throws NameException {
+		Integer expected = 4;
+		Integer rating = expected;
+		String firstName = "Alex";
+		String lastName = "Bo";
+		Student s = new Student(firstName, lastName, rating);
+		Integer actual = s.getRating();
+		assertEquals(expected, actual);
+	}
+
+}
